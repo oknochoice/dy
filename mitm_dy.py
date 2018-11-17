447277b07ffce86ddb4338fbfcebe5edbc5e115a
oknochoice <oknochoice@gamil.com>
2018-11-18 05:10:00 +0800

    final

--- mitm_dy.py
+++ mitm_dy.py
@@ -11,6 +11,7 @@ import logging
 
 class Sql_dy:
     def __init__(self):
+        logging.debug('%s begin', __class__)
         self.conn = sqlite3.connect('dy.db')
         self.conn.execute("create table if not exists t_users (id integer primary key autoincrement, short_id text not null, uid text unique not null, nickname text, gender int not null, location text, following_count int not null, follower_count int not null, aweme_count int not null, total_favorited int not null)")
         self.conn.execute("create index if not exists uid on t_users (uid)")
@@ -24,32 +25,41 @@ class Sql_dy:
         self.conn.execute("create table if not exists t_user_avatars (id integer primary key autoincrement, uid text not null, md5 text not null, ts int not null, unique(uid, md5))")
         self.conn.commit()
     def __del__(self):
-        Sql_dy.conn.close()
+        logging.debug('%s begin', __class__)
+        self.conn.close()
     
     def insertUser(self, para):
+        logging.debug('%s begin', __class__)
         sql = '''insert or replace into t_users (short_id, uid, nickname, gender, location, following_count, follower_count, aweme_count , total_favorited) values (?,?,?,?,?,?,?,?,?)'''
         self.conn.execute(sql, para)
         self.conn.commit()
     def insertVideoFailed(self, para):
+        logging.debug('%s begin', __class__)
         sql = '''insert or ignore into t_videos_failed (url, web_hash, uid, ts, statistics) values (?,?,?,?,?)'''
         self.conn.execute(sql, para)
         self.conn.commit()
     def insertVideo(self, para):
+        logging.debug('%s begin', __class__)
         sql_del = '''delete from t_videos_failed where web_hash = ?'''
         sql = '''insert or ignore into t_videos (md5, web_hash, uid, ts, statistics) values (?,?,?,?,?)'''
-        self.conn.execute(sql_del, (para[1]))
+        self.conn.execute(sql_del, (para[1],))
         self.conn.execute(sql, para)
         self.conn.commit()
     def insertAvatars(self, para):
+        logging.debug('%s begin', __class__)
         sql = '''insert or ignore into t_user_avatars (md5, uid, ts) values(?,?,?)'''
         self.conn.execute(sql, para)
         self.conn.commit()
     def isVideoExist(self, para) -> bool:
+        logging.debug('%s begin', __class__)
+        logging.info('para %s', para.__str__())
         sql = '''select web_hash from t_videos where web_hash = ?'''
         res = self.conn.execute(sql, para).fetchone()
         if res is None:
+            logging.info('video is exist')
             return False
         else:
+            logging.info('video is not exist')
             return True
 
 
@@ -77,7 +87,8 @@ class DownloadThread:
             self.statistics = statistics
         def run(self):
             logging.debug('%s begin', __class__)
-            if Singleton_sql.get_instance().isVideoExist(self.webhash):
+            if Singleton_sql.get_instance().isVideoExist((self.webhash,)):
+                logging.info('%s video is exist', __class__)
                 pass
             else:
                 try:
@@ -92,6 +103,7 @@ class DownloadThread:
                     f.write(res.content)
                     f.flush()
                 param = (md5.hexdigest(), self.webhash, self.uid, self.ts, self.statistics)
+                logging.info(param.__str__())
                 Singleton_sql.get_instance().insertVideo(param)
     class Avatar(Source):
         def __init__(self, url, uid):
@@ -174,41 +186,41 @@ class TrackRecorder:
 
 class UserInfoer:
     def load(self, entry: addonmanager.Loader):
+        ctx.log.alert('load begin')
+        logging.getLogger('').handlers = []
         log_format = "[%(asctime)s:%(levelname)s:%(thread)d:%(filename)s:%(lineno)d:%(funcName)s]%(message)s"
         logging.basicConfig(filename='my.log', level=logging.DEBUG, format=log_format)
-        logging.debug('%slog init test', __class__)
+        logging.getLogger("requests").setLevel(logging.WARNING)
+        logging.getLogger("urllib3").setLevel(logging.WARNING)
+        logging.getLogger("wsgi").setLevel(logging.WARNING)
+        logging.debug('%s log init test', __class__)
     def response(self, flow: http.HTTPFlow) -> None:
-        #ctx.log.info('userinfo runing -------------------------------')
-        #ctx.log.info(flow.request.url)
+        logging.info('in %s',flow.request.url)
         if (flow.request.url.startswith('https://aweme-eagle.snssdk.com/aweme/v1/user') or
             flow.request.url.startswith('https://aweme.snssdk.com/aweme/v1/user')):
+            logging.info('user info url %s',flow.request.url)
             # insert user
             data = json.loads(flow.response.get_text())
             user = data['user']
             user_info = (user['short_id'], user['uid'], user['nickname'], user['gender'], user['location'], user['following_count'], user['follower_count'], user['aweme_count'], user['total_favorited'])
-            ctx.log.info(user_info.__str__())
             u_task = DownloadThread.Userinfo(user_info)
+            logging.info(user_info.__str__())
             Singleton_tread.get_instance().addTasks([u_task])
             # insert avatar
             avatar_url = user['avatar_larger']['url_list'][0]
             a_task = DownloadThread.Avatar(avatar_url, user['uid'])
+            logging.info((avatar_url, user['uid']).__str__())
             Singleton_tread.get_instance().addTasks([a_task])
         if flow.request.url.find('/aweme/v1/aweme/post/') != -1:
-            #ctx.log.info('-------------------------------')
-            #ctx.log.info(flow.request.query.fields.__str__())
-            ctx.log.info(flow.request.url)
+            logging.info('user produce url %s',flow.request.url)
             res_dict = json.loads(flow.response.get_text())
-            #ctx.log.info(flow.request.url)
             fields = flow.request.query.fields
-            #ctx.log.info(fields.__str__())
             for tt in enumerate(fields):
                 #ctx.log.info(tt.__str__())
                 if tt[1][0] == 'user_id':
                     self.uid = tt[1][1]
                 elif tt[1][0] == 'ts':
                     self.ts = tt[1][1]
-            ctx.log.info(self.uid)
-            ctx.log.info(self.ts)
             video_list = res_dict['aweme_list']
             videos = []
             for video in enumerate(video_list):
@@ -216,6 +228,7 @@ class UserInfoer:
                 urlDict = urlparse(url)
                 statistics = video[1]['statistics']
                 v = DownloadThread.Video(url, urlDict.path, self.uid, self.ts, json.dumps(statistics))
+                logging.info((url, urlDict.path, self.uid, self.ts, json.dumps(statistics)).__str__())
                 videos.append(v)
             Singleton_tread.get_instance().addTasks(videos)
 
