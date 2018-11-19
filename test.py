9491947b0d8e022841e0f3bf8ea70238a96b03f1
oknochoice <oknochoice@gamil.com>
2018-11-20 05:41:00 +0800

    fixed

---
+++ test.py
@@ -0,0 +1,20 @@
+import re
+
+if __name__ == "__main__":
+    s1 = '12.3W粉丝'
+    s2 = '12.3w粉丝'
+    s3 = '123W粉丝'
+    s4 = '123W粉丝'
+    r1 = s1.find('.')
+    r2 = s2.find('.')
+    r3 = s3.find('.')
+    r4 = s4.find('.')
+    print("%d, %d, %d, %d", r1, r2, r3, r4 )
+    s1 = re.sub(r'\..*', '', s1)
+    s2 = re.sub(r'\..*', '', s2)
+    s3 = re.sub(r'\..*', '', s3)
+    s4 = re.sub(r'\..*', '', s4)
+    print("%s", s1)
+    print("%s", s2)
+    print("%s", s3)
+    print("%s", s4)
